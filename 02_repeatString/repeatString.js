38bac9742661de5ea777ef8a299284818aa3e3c5
Tornak9 <Tornak9@gmail.com>
2022-07-28 23:31:02 +0200

    Finish the repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, numberOfRepeats) {
+    let repeatedString = '';
+    if (numberOfRepeats > 0) {
+        for (let i = 0; i < numberOfRepeats; i++) {
+            repeatedString += string;
+        }
+    } else if (numberOfRepeats == 0) {
+        return repeatedString
+    } else if (numberOfRepeats < 0) {
+        repeatedString = "ERROR";
+    }
+    return repeatedString;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
