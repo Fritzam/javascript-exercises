0587f7719def7a277e3e49bee0d993d21323e021
Tornak9 <Tornak9@gmail.com>
2022-07-30 00:19:24 +0200

    Made working function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
-
-};
-
+const sumAll = function(min_value, max_value) {
+    if (typeof min_value === 'number' && typeof max_value === 'number') {
+        if (min_value > 0 && max_value >= 0) {
+            if (min_value > max_value) {
+                let sum = 0;
+                for (let i = max_value; i <= min_value; i++) {
+                    sum += i;
+                }
+                return sum;
+            } else if (max_value > min_value) {
+                let sum = 0;
+                for (let i = min_value; i <= max_value; i++) {
+                    sum += i;
+                }
+                return sum;
+            }
+        } else {
+            return "ERROR";
+        }
+    } else {
+        return "ERROR";
+    }
+}
 // Do not edit below this line
 module.exports = sumAll;
