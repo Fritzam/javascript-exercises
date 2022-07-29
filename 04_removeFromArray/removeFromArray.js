e5c0c9c155c879d4649c9ceaa64509bb5e58acd0
Tornak9 <Tornak9@gmail.com>
2022-07-29 20:17:21 +0200

    Remove skips from test and wrote working function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,29 @@
-const removeFromArray = function() {
+/*const removeFromArray = function(array, ...elemsToRemove) {
+    let removedArray = [];
+    let toRemoveArray = [...elemsToRemove];
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < toRemoveArray.length; i++) {
+            if (array[i].indexOf(toRemoveArray[j]) === -1) {
+                removedArray.push(array[i]);
+            } else {
+                continue;
+            }
+        }
+    }
+    return removedArray;
+
+};*/
+const removeFromArray = function(array, ...elemsToRemove) {
+    let removedArray = [];
+    let toRemoveArray = [...elemsToRemove];
+    for (let i = 0; i < array.length; i++) {
+        if (toRemoveArray.indexOf(array[i]) === -1) {
+            removedArray.push(array[i]);
+        } else {
+            continue;
+        }
+    }
+    return removedArray;
 
 };
 
