5e130e989158ff6ebfc08aef87b03a41cc2ced38
Tornak9 <Tornak9@gmail.com>
2022-07-30 15:16:18 +0200

    Made working function

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,49 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let result = 0;
+  for (let i = 0; i < array.length; i++) {
+      result += array[i];
+  }
+
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = 1;
+  for (let i = 0; i < array.length; i++) {
+    result *= array[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+  let result = 1;
+  if (power === 0) {
+    return "1";
+  }
+  for (let i = 0; i < power; i++) {
+    result *= number;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(number) {
+  if (number === 0) {
+    return 1;
+  }
+  let result = 1;
+  for (let i = number; i > 0; i--) {
+    result *= i;
+  }
+  return result;
+}
 
 // Do not edit below this line
 module.exports = {
