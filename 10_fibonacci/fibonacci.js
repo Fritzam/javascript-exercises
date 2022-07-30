30692db338228b3a6551b603c1e4907c7229f74f
Tornak9 <Tornak9@gmail.com>
2022-07-30 19:27:41 +0200

    Made working Fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(numberInSequence) {
+    if(numberInSequence < 0) {
+        return "OOPS";
+    }
 
+    let requestedNumber = [0, 1];
+    for (let i = 2; i <= numberInSequence; i++) {
+        requestedNumber.push(requestedNumber[i-2] + requestedNumber[i-1]);
+    }
+    return requestedNumber[numberInSequence];
 };
 
 // Do not edit below this line
